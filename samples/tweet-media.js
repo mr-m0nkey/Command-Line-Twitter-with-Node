026d8d46af118e68cbda893238af4bdb2143ea56
Mayowa Aj <sagaraaj69@gmail.com>
2020-01-13 13:55:39 +0100

    some sample json data

---
+++ samples/tweet-media.js
@@ -0,0 +1,24 @@
+[ { id: 1216442736263348200,
+    id_str: '1216442736263348225',
+    indices: [ 22, 45 ],
+    media_url: 'http://pbs.twimg.com/media/EOGsOUCUwAE1C9l.jpg',
+    media_url_https: 'https://pbs.twimg.com/media/EOGsOUCUwAE1C9l.jpg',
+    url: 'https://t.co/gK3gpO8NpY',
+    display_url: 'pic.twitter.com/gK3gpO8NpY',
+    expanded_url:
+     'https://twitter.com/LilNasX/status/1216442744257703936/photo/1',
+    type: 'photo',
+    sizes:
+     { thumb: [Object],
+       small: [Object],
+       medium: [Object],
+       large: [Object] },
+    source_status_id: 1216442744257704000,
+    source_status_id_str: '1216442744257703936',
+    source_user_id: 754006735468261400,
+    source_user_id_str: '754006735468261376',
+    features:
+     { orig: [Object],
+       small: [Object],
+       medium: [Object],
+       large: [Object] } } ]
